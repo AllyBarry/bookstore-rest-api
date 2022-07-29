835f625ed36082e2121fc0ae4d3269413857385b
Alexandra <alexandra@elenjicalsolutions.com>
2022-07-29 16:08:25 +0200

    Initial database load and setup

---
+++ initialize_data.py
@@ -0,0 +1,60 @@
+import csv
+import os
+import logging
+from faker import Faker
+
+fake = Faker()
+
+from persistence.database import SessionLocal, engine
+from persistence import schemas, crud, models
+
+script_dir = os.path.dirname(os.path.realpath(__file__))
+sample_users = os.path.join(script_dir, "sample-data", "users.csv")
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def load_sample_data(db: SessionLocal):
+    # Loads test data into the database
+    with open(sample_users, "r") as f:
+        csv_reader = csv.DictReader(f, delimiter=';')
+
+        for row in csv_reader:
+            print(row)
+            user = crud.get_user_by_email(db, email=row["email"])
+            if not user:
+                    user_in = schemas.UserCreate(
+                        email=row["email"],
+                        password=row["password"],
+                        author_name=row["author_name"],
+                        is_superuser=False,
+                    )
+                    user = crud.create_user(db, user_in)
+
+            for _ in range(3):
+                new_book = schemas.BookCreate(
+                    title=fake.sentence(4),
+                    description= fake.sentence(10)
+                )
+                crud.create_user_book(db, new_book, user.id)
+
+def init_db(db: SessionLocal):
+
+    models.Base.metadata.create_all(bind=engine)
+
+    load_sample_data(db)
+
+
+def init():
+    db = SessionLocal()
+    init_db(db)
+
+
+def main():
+    logger.info("Creating initial data")
+    init()
+    logger.info("Initial data created")
+
+
+if __name__ == "__main__":
+    main()
---
+++ persistence/schemas.py
@@ -0,0 +1,38 @@
+from typing import List, Union
+
+from pydantic import BaseModel
+
+
+class BookBase(BaseModel):
+    title: str
+    description: Union[str, None] = None
+
+
+class BookCreate(BookBase):
+    pass
+
+
+class Book(BookBase):
+    id: int
+    author_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class UserBase(BaseModel):
+    email: str
+    author_name: str
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class User(UserBase):
+    id: int
+    is_active: bool
+    books: List[Book] = []
+
+    class Config:
+        orm_mode = True
